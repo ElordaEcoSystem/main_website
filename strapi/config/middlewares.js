340cbbb8e6bbc74accbfa6541a2d937925f542f5
new name <NAmirov@elordaeco.kz>
2024-04-11 18:26:30 +0500

    back to 10mb limit for strapi

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -5,19 +5,19 @@ module.exports = [
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
-  // 'strapi::body',
+  "strapi::body",
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
-  {
-    name: "strapi::body",
-    config: {
-      formLimit: "1000mb", // modify form body
-      jsonLimit: "1000mb", // modify JSON body
-      textLimit: "1000mb", // modify text body
-      formidable: {
-        maxFileSize: 1024 * 1024 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
-      },
-    },
-  },
+  // {
+  //   name: "strapi::body",
+  //   config: {
+  //     formLimit: "1000mb", // modify form body
+  //     jsonLimit: "1000mb", // modify JSON body
+  //     textLimit: "1000mb", // modify text body
+  //     formidable: {
+  //       maxFileSize: 1024 * 1024 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+  //     },
+  //   },
+  // },
 ];
